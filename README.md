6107ae336fd6160dfe682851f3435300c90b6e18
Wytamma Wirth <wytamma.wirth@me.com>
2021-05-26 15:46:20 +1000

    :art: move example to README

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # ESS predict 
 
+
+```R
+source('ESSpredict.R')
+log <- read.logFile('logs/4.2.1-het-strict.aln.log')
+target <- 200
+burnin <- 0.1
+essdf <- cumESS(log, cores = 6, burnin = burnin)
+plotESS(essdf)
+```
 ![](figures/1.png)
 
-
+```R
+fit <- stepsUntil(essdf, target)
+Statistic_below_ESS <- setdiff(unique(essdf$Statistic), unique(essdf[essdf$ESS > target,]$Statistic))
+for (stat in Statistic_below_ESS) {
+  p <- plotESS(
+      essdf[essdf$Statistic == stat,][essdf[essdf$Statistic == stat,]$ESS < target, ], 
+      fit=fit[fit$Statistic == stat,])
+  print(p)
+}
+```
 ![](figures/2.png)
--- test.R
+++ test.R
@@ -1,18 +1,3 @@
-source('ESSpredict.R')
-
-log <- read.logFile('/Users/wytamma/programming/typhi-in-fiji/skygrid/4.2.2-skygrid.aln.log')
-cutoff <- 1e8
-target <- 200
-burnin <- 0.1
-essdf <- cumESS(log, cores = 6, burnin = burnin)
-plotESS(essdf)
-fit <- stepsUntil(essdf, target)
-Statistic_below_ESS <- setdiff(unique(essdf$Statistic), unique(essdf[essdf$ESS > target,]$Statistic))
-for (stat in Statistic_below_ESS) {
-  p <- plotESS(essdf[essdf$Statistic == stat,][essdf[essdf$Statistic == stat,]$ESS < target, ], cutoff=cutoff, fit=fit[fit$Statistic == stat,])
-  print(p)
-}
-
 l <- list()
 
 # do a first passs ESS of the full chain to see which statistics have EEE < target
